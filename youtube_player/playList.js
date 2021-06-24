6262d2ad055de3df61b88ff0f2fe5d9689c687eb
allen <a09a03@mail.com>
2021-06-25 00:32:01 +0800

    First commit

---
+++ youtube_player/playList.js
@@ -0,0 +1,15 @@
+//Video ID
+let playList = [
+    "cm-l2h6GB8Q",
+    "JP28qMAXBa8",
+    "Zxa3CPFCtpI",
+    "S4JLJVVjevI", 
+];
+
+//start sec
+let playTime = [
+    [52,65],
+    [71,99],
+    [63,86],
+    [53,79],
+];
